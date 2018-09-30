c931c778bd03ffc985b38c5a68309e07c0825f3d
EgorDudyrev <egor.dudyrev@yandex.ru>
2018-09-30 13:51:19 +0300

    Load some songs from chordlib

--- notebooks/chord_rec_lib.py
+++ notebooks/chord_rec_lib.py
@@ -1,10 +1,15 @@
 import re
+import os
+
+# Максимальная разница в секундах между скачанной песней и её размеченной версией
+MAX_DUR_DELTA = 2
 
 # Пути к различным папкам из корневой директории проекта
 dnames = {'BB_DS_DIR':'base_data\\McGill-Billboard',
           'BB_PARS_DS_DIR':'base_data/billboard-2.0.1-lab\\McGill-Billboard',
           'CL_DS_DIR':'base_data\\chordlab',
-          'CSVS_DIR':'csvs'}
+          'CSVS_DIR':'csvs',
+          'RAW_SONGS_DIR':'raw_songs'}
  
 def format_name(s, space_replacer='_'):
     """
@@ -16,10 +21,30 @@ def format_name(s, space_replacer='_'):
     * Отформатированная строка s
     """
     s = s.lower()
-#    for c in ['(',')','\'']:
-#        s = s.replace(c,'')
+    for c in ['(',')','\'', '.', '\`']:
+        s = s.replace(c,'')
     s = re.sub(r"\(\)\'\,\`", '', s)
+    s = re.sub(r'\bthe\b','',s)
+    s = re.sub(r'\ba\b','',s)
+    s = re.sub(r'\ban\b','',s)
     s = re.sub('&', 'and', s)
     s = s.strip(' ')
     s = s.replace(' ', space_replacer)
     return s
+
+def find_files(dname, frmt=None):
+    """
+    Поиск всех файлов определённого (опционально) формата в папке dname
+    """
+    files = []
+    for f in os.listdir(dname):
+        fname = os.path.join(dname, f) 
+        if os.path.isfile(fname):
+            if frmt:
+                if fname.endswith(frmt):
+                    files.append(fname)
+            else:
+                files.append(fname)
+        else:
+            files += find_files(fname, frmt)
+    return files
